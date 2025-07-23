e038bbe755aa17865fdb65bf01637e440c657829
Armank19 <armankzrg.10@gmail.com>
2025-07-23 17:30:42 +0700

    Update 2025_06_29_024646_create_pesertas_table.php

--- database/migrations/2025_06_29_024646_create_pesertas_table.php
+++ database/migrations/2025_06_29_024646_create_pesertas_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
         $table->string('email')->nullable();
         $table->string('no_wa')->nullable();
         $table->boolean('status_checkin')->default(false);
-        $table->integer('nomor_undian', 20)->nullable();
+        $table->string('nomor_undian', 20)->nullable();
         $table->timestamps();
     });
 }
